1fc230838497d55f38c2602ac0993ad24e7dafb2
Gyalla <Gyallahorn@gmail.com>
2020-03-17 17:21:23 +0900

    commit

--- lib/map/marker_list.dart
+++ lib/map/marker_list.dart
@@ -1,21 +1,20 @@
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:pospredsvto/mam/mark_marker.dart';
 
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:pospredsvto/mam/mark_marker.dart';
+// final List<MapMarker> markers = [];
 
-final List<MapMarker> markers = [];
+// final List<LatLng> markerLocations = [
+//    LatLng(62.016919, 129.704180),
+//    LatLng(62.034125,129.713635),
 
-final List<LatLng> markerLocations = [
-   LatLng(62.016919, 129.704180),
-   LatLng(62.034125,129.713635),
-   
-];
+// ];
 
-for (LatLng markerLocation in _markerLocations) {
-   markers.add(
-      MapMarker(
-         id: markerLocations.indexOf(markerLocation).toString(),
-         position: markerLocation,
-         icon: markerImage,
-      ),
-   );
-}
+// for (LatLng markerLocation in _markerLocations) {
+//    markers.add(
+//       MapMarker(
+//          id: markerLocations.indexOf(markerLocation).toString(),
+//          position: markerLocation,
+//          icon: markerImage,
+//       ),
+//    );
+// }
