4b3875fd185c81699b077a490e524c5aed60aa75
Gyalla <Gyallahorn@gmail.com>
2020-03-19 17:35:11 +0900

    little refactor

--- lib/main/tab_navigation.dart
+++ lib/main/tab_navigation.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:pospredsvto/main.dart';
+import 'package:pospredsvto/map/map.dart';
 
 class NavigationTab {
   final List<Widget> widgetList = [
-    MyApp(),
+    MapFrame(),
     Icon(Icons.person),
     Icon(Icons.remove_red_eye),
   ];
