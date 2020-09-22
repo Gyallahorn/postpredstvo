e21f7357b6df719e834b25cac549c62cdf4873dd
Gyalla <Gyallahorn@gmail.com>
2020-09-23 05:08:50 +0900

    almost cool with edited url

--- lib/network/url_helper.dart
+++ lib/network/url_helper.dart
@@ -2,5 +2,5 @@ const kSignUpURL = 'https://postpredstvo-back.herokuapp.com/api/user/signup';
 const kLoginCreateTokerURL =
     'https://postpredstvo-back.herokuapp.com/api/user/signin';
 
-// const urlHost = 'https://postpredstvo-back.herokuapp.com';
-const urlHost = 'http://192.168.0.130:4000';
+const urlHost = 'https://postpredstvo-back.herokuapp.com';
+// const urlHost = 'http://192.168.0.130:4000';
